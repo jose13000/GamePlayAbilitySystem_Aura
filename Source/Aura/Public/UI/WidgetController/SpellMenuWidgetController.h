a1d3dec60d392b80bfc04a438d1c5ec9b85ba2fb
Jose Ramirez <josram13000@gmail.com>
2023-11-29 15:33:20 -0500

    Selected Ability

--- Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -10,6 +10,11 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FSpellGlobeSelectedSignature, bool, bSpendPointsButtonEnabled, bool, bEquipButtonEnabled);
 
+struct FSelectedAbility
+{
+	FGameplayTag Ability = FGameplayTag();
+	FGameplayTag Status = FGameplayTag();
+};
 /**
  * 
  */
@@ -33,4 +38,6 @@ public:
 private:
 
 	static void ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints, bool& bShouldEnableSpellPointsButton, bool& bShouldEnableEquipButton);
+	FSelectedAbility SelectedAbility = { FAuraGameplayTags::Get().Abilities_None,  FAuraGameplayTags::Get().Abilities_Status_Locked };
+	int32 CurrentSpellPoints = 0;
 };
