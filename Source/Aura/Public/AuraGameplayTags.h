c4c2f9d6df8ccb741049ebe1679409a353e53323
Jose Ramirez <josram13000@gmail.com>
2023-12-01 22:02:48 -0500

    Debuff Parameters

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -78,6 +78,11 @@ public:
 	FGameplayTag Debuff_Arcane;
 	FGameplayTag Debuff_Physical;
 
+	FGameplayTag Debuff_Chance;
+	FGameplayTag Debuff_Damage;
+	FGameplayTag Debuff_Duration;
+	FGameplayTag Debuff_Frequency;
+
 	/*
 	 * Abilities
 	 */
